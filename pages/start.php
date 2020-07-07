01156d8b9459c27c35689dd022733730dbde7b30
Ivan Ivković <ivan.ivkovic1996@gmail.com>
2020-07-07 18:06:54 +0200

    Poveznice prema profilu i postavkama.

--- pages/start.php
+++ pages/start.php
@@ -1,4 +1,33 @@
-<?php 
-include '../scripts/functions.php'; 
-    echo 'Prijava uspješna!';
-    ?>
+<?php include '../scripts/functions.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ribogram – Početak rada</title>
+    <link rel="stylesheet" href="../styles/style.css">
+</head>
+
+<body>
+    <main>
+
+    <article>
+    <div id="start_head">
+            <!-- <img class="logo" src="../images/logo.png" /> -->
+			<ul>
+                <li><a href="profile.php">Profil</a></li>
+                <li><a href="settings.php">Postavke</a></li>
+			</ul>
+    <div/>
+    </article>
+
+    <article>
+        <h1>Galerija</h1>
+    </article>
+
+    </main>
+
+</body>
+
+</html>
