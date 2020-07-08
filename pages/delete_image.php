8fb39d9e6858fcd19ffab301274dc968a0f5863f
Ivan Ivković <ivan.ivkovic1996@gmail.com>
2020-07-08 12:28:27 +0200

    Brisanje datoteke iz direktorija.

--- pages/delete_image.php
+++ pages/delete_image.php
@@ -1,6 +1,12 @@
 <?php include '../scripts/connect.php';
 
     $id_image = $_GET['id_image'];
+    $image = "SELECT * FROM images WHERE id_image=$id_image";
+    $res = mysqli_query($db, $image);
+    $row = mysqli_fetch_assoc($res);
+    $name = $row['image'];
+    unlink("../images/" .$name);
+
     $sql = "DELETE from images WHERE id_image='$id_image'";
     mysqli_query($db, $sql);
     header('location: myprofile.php');
